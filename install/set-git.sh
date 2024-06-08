aa5a26984d9ea4ecf191a50db589a7747bfa0b1a
Nilesh <nilesh@cloudgeni.us>
2024-06-07 23:29:07 -0700

    ulauncher and tactile

--- install/set-git.sh
+++ install/set-git.sh
@@ -4,3 +4,5 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global pull.rebase true
+git config --global user.email nilesh@cloudgeni.us
+git config --global user.name Nilesh
