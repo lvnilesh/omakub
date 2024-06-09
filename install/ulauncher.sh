0c4f804318aa7740e25721f99a40c56367cdc2f7
Nilesh <nilesh@cloudgeni.us>
2024-06-09 10:28:05 -0700

    make keyd optional

--- install/ulauncher.sh
+++ install/ulauncher.sh
@@ -5,7 +5,9 @@ sudo apt install -y ulauncher
 
 # Start ulauncher to have it populate config before we overwrite
 mkdir -p ~/.config/autostart/
-cp ~/.local/share/omakub/configs/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
+rm -rf ~/.config/autostart/ulauncher.desktop
+ln -s ~/.local/share/omakub/configs/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
 gtk-launch ulauncher.desktop >/dev/null 2>&1
 sleep 2 # ensure enough time for ulauncher to set defaults
-cp ~/.local/share/omakub/configs/ulauncher.json ~/.config/ulauncher/settings.json
+rm -rf ~/.config/ulauncher/settings.json
+ln -s ~/.local/share/omakub/configs/ulauncher.json ~/.config/ulauncher/settings.json
