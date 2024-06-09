40ced322050440b7e84af72b7ccccb70f7f58d31
Nilesh <nilesh@cloudgeni.us>
2024-06-09 10:30:29 -0700

    quieter gum install

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt install -y curl git jq unzip
 
 # install gum
 sudo mkdir -p /etc/apt/keyrings
+sudo rm -rf /etc/apt/keyrings/charm.gpg
 curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
 sudo rm -rf /etc/apt/sources.list.d/charm.list
 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
