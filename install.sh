f59bf43ea31a86b8ff2759ac4a01d000e3f7246e
Nilesh <nilesh@cloudgeni.us>
2024-06-09 07:46:36 -0700

    install gum - not available on fresh installs

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ source ~/.local/share/omakub/ascii.sh
 sudo apt update -y
 sudo apt install -y curl git jq unzip
 
+# install gum
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+sudo rm -rf /etc/apt/sources.list.d/charm.list
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install gum
+
 # Ensure computer doesn't go to sleep or lock while installing
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
